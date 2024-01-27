cfde0b1e2ae0cbe356dff11ddce6d9cbd3c85921
Ruben Dougall <Ruben9922@users.noreply.github.com>
2024-01-27 18:04:23 +0000

    Use new React 18 root API

--- web-app/src/index.tsx
+++ web-app/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import "./index.css";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
@@ -24,14 +24,15 @@ const theme = extendTheme({
   }
 });
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
